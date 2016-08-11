47a8b1e48e5ed38ba349fa307a0f8a3beaa94a6f
arikyudin <yudarik@gmail.com>
2016-08-12 00:40:48 +0300

    many fixes

--- src/app/core/components/charts/user-quota.component.js
+++ src/app/core/components/charts/user-quota.component.js
@@ -7,38 +7,60 @@
 
     angular.module('Simulator.components')
         .component('userQuota', {
-            template: '<div>'+
-                      '<canvas id="pie" class="chart chart-horizontal-bar"'+
-                      'chart-data="$ctrl.data" '+
-                      'chart-labels="$ctrl.labels" '+
-                      'chart-series="$ctrl.series"'+
-                      'chart-options="$ctrl.options">'+
-                      '</canvas>'+
-                      '</div>',
+            template: '<div class="col-xs-6">'+
+                          '<canvas class="chart chart-pie"'+
+                          'chart-data="$ctrl.questionsQuota.data" '+
+                          'chart-labels="$ctrl.questionsQuota.labels" '+
+                          'chart-series="$ctrl.questionsQuota.series"'+
+
+                          'chart-options="$ctrl.options">'+
+                          '</canvas>'+
+                      '</div>'+
+                        '<div class="col-xs-6">'+
+                        '<canvas class="chart chart-pie"'+
+                        'chart-data="$ctrl.practicesPerformed.data" '+
+                        'chart-labels="$ctrl.practicesPerformed.labels" '+
+                        'chart-options="$ctrl.options">'+
+                        '</canvas>'+
+                        '</div>',
             controller: userQuotaCtrl
         });
 
     /** @ngInject */
     function userQuotaCtrl($translate, customerStatsService) {
-
-        this.labels = [];
-        this.data = [];
-        this.series = [];
         this.options = {
             legend: {
-                display: false,
-                position: 'top'
+                display: true,
+                position: 'bottom'
             }
         };
 
+        this.questionsQuota = {
+            labels: [],
+            data: []
+        };
+        this.practicesPerformed = {
+            labels: [],
+            data: []
+        };
+
         customerStatsService.getQuota().then((quota) => {
-            _.forEach(quota, (val, key) => {
-                if (typeof val === 'number') {
-                    this.labels.push($translate.instant('STATS.ACCOUNT.'+key.toUpperCase()));
-                    //this.series.push(key);
-                    this.data.push(val);
-                }
-            })
+
+            this.questionsQuota.data = [
+                quota['totalNewQuestionsQuota'] - quota['leftNewQuestionsQuota'],
+                quota['leftNewQuestionsQuota']
+            ];
+            this.questionsQuota.labels[0] = $translate.instant('STATS.ACCOUNT.SPENTNEWQUESTIONSQUOTA');
+            this.questionsQuota.labels[1] = $translate.instant('STATS.ACCOUNT.LEFTNEWQUESTIONSQUOTA');
+
+            this.practicesPerformed.data = [
+                quota['totalPostCreditQuestionsQuota'] - quota['leftPostCreditQuestionsQuota'],
+                quota['leftPostCreditQuestionsQuota']
+            ];
+            this.practicesPerformed.labels[0] = $translate.instant('STATS.ACCOUNT.SPENTPOSTCREDITQUESTIONSQUOTA');
+            this.practicesPerformed.labels[1] = $translate.instant('STATS.ACCOUNT.LEFTPOSTCREDITQUESTIONSQUOTA');
+
+
         });
     }
 
